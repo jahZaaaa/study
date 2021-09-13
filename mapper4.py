b27a402b2d69c822ccfe3239bdff4d3196cff5e2
jahZaaaa <43313123+jahZaaaa@users.noreply.github.com>
2021-09-13 22:51:02 +0700

    Add files via upload

---
+++ mapper4.py
@@ -0,0 +1,49 @@
+import sys
+lines = []
+var = sys.stdout
+tow=[]
+#with open('note.txt') as f:
+    #lines = f.readlines()
+
+
+count = 0
+for line in sys.stdin:
+#for line in lines:
+    line = line.strip()
+    rows = line.split(",")
+    (name1,phone1,name2,phone2,timestamp_ms,timestamp_str,duration_m) = tuple(rows)
+    #print(rows)
+    
+    
+    #for i in rows:
+    #    var.write(i)
+    
+    #tow.append('{}\t{}'.format(name1, int(duration_m)))
+    
+	#(name1,phone1,name2,phone2,timestamp_ms,timestamp_str,duration_m) = tuple(rows)
+
+	# write the results to STDOUT (standard output);
+	# what we output here will be the input for the
+	# Reduce step, i.e. the input for reducer.py
+
+    
+    try:
+        #print('{}\t{}'.format(name1, int(duration_m)))
+        var.write('{}\t{}'.format(name1, int(duration_m)))
+        var.write('\n')
+        vv=0 
+        
+    except ValueError as e:
+                      
+        print('')
+        continue
+
+#f.close()
+###############
+
+
+
+
+
+
+	
